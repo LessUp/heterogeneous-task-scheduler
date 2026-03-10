2981ec31347ce57cd9ce306744a171f12b99b264
shijiashuai <jiashuai.shi@qq.com>
2026-03-10 14:10:54 +0800

    fix: resolve CI workflow failures

--- include/hts/task_builder.hpp
+++ include/hts/task_builder.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "hts/types.hpp"
 #include "hts/task.hpp"
 #include "hts/task_graph.hpp"
+#include "hts/types.hpp"
 #include <memory>
 #include <string>
 
@@ -11,95 +11,95 @@ namespace hts {
 /// Fluent builder for creating tasks
 class TaskBuilder {
 public:
-    explicit TaskBuilder(TaskGraph& graph) : graph_(graph) {}
-    
-    /// Set task name
-    TaskBuilder& name(const std::string& name) {
-        name_ = name;
-        return *this;
-    }
-    
-    /// Set preferred device
-    TaskBuilder& device(DeviceType device) {
-        device_ = device;
-        return *this;
-    }
-    
-    /// Set task priority
-    TaskBuilder& priority(TaskPriority priority) {
-        priority_ = priority;
-        return *this;
-    }
-    
-    /// Set CPU function
-    TaskBuilder& cpu(Task::CpuFunction func) {
-        cpu_func_ = std::move(func);
-        return *this;
-    }
-    
-    /// Set GPU function
-    TaskBuilder& gpu(Task::GpuFunction func) {
-        gpu_func_ = std::move(func);
-        return *this;
-    }
-    
-    /// Add dependency on another task
-    TaskBuilder& after(TaskId id) {
-        dependencies_.push_back(id);
-        return *this;
-    }
-    
-    /// Add dependency on another task
-    TaskBuilder& after(const std::shared_ptr<Task>& task) {
-        dependencies_.push_back(task->id());
-        return *this;
-    }
-    
-    /// Build and add task to graph
-    std::shared_ptr<Task> build() {
-        auto task = graph_.add_task(device_);
-        
-        if (!name_.empty()) {
-            task->set_name(name_);
-        }
-        
-        task->set_priority(priority_);
-        
-        if (cpu_func_) {
-            task->set_cpu_function(std::move(cpu_func_));
-        }
-        
-        if (gpu_func_) {
-            task->set_gpu_function(std::move(gpu_func_));
-        }
-        
-        for (TaskId dep : dependencies_) {
-            graph_.add_dependency(dep, task->id());
-        }
-        
-        // Reset for next build
-        reset();
-        
-        return task;
+  explicit TaskBuilder(TaskGraph &graph) : graph_(graph) {}
+
+  /// Set task name
+  TaskBuilder &name(const std::string &name) {
+    name_ = name;
+    return *this;
+  }
+
+  /// Set preferred device
+  TaskBuilder &device(DeviceType device) {
+    device_ = device;
+    return *this;
+  }
+
+  /// Set task priority
+  TaskBuilder &priority(TaskPriority priority) {
+    priority_ = priority;
+    return *this;
+  }
+
+  /// Set CPU function
+  TaskBuilder &cpu(Task::CpuFunction func) {
+    cpu_func_ = std::move(func);
+    return *this;
+  }
+
+  /// Set GPU function
+  TaskBuilder &gpu(Task::GpuFunction func) {
+    gpu_func_ = std::move(func);
+    return *this;
+  }
+
+  /// Add dependency on another task
+  TaskBuilder &after(TaskId id) {
+    dependencies_.push_back(id);
+    return *this;
+  }
+
+  /// Add dependency on another task
+  TaskBuilder &after(const std::shared_ptr<Task> &task) {
+    dependencies_.push_back(task->id());
+    return *this;
+  }
+
+  /// Build and add task to graph
+  std::shared_ptr<Task> build() {
+    auto task = graph_.add_task(device_);
+
+    if (!name_.empty()) {
+      task->set_name(name_);
     }
 
-private:
-    void reset() {
-        name_.clear();
-        device_ = DeviceType::Any;
-        priority_ = TaskPriority::Normal;
-        cpu_func_ = nullptr;
-        gpu_func_ = nullptr;
-        dependencies_.clear();
+    task->set_priority(priority_);
+
+    if (cpu_func_) {
+      task->set_cpu_function(std::move(cpu_func_));
     }
-    
-    TaskGraph& graph_;
-    std::string name_;
-    DeviceType device_ = DeviceType::Any;
-    TaskPriority priority_ = TaskPriority::Normal;
-    Task::CpuFunction cpu_func_;
-    Task::GpuFunction gpu_func_;
-    std::vector<TaskId> dependencies_;
+
+    if (gpu_func_) {
+      task->set_gpu_function(std::move(gpu_func_));
+    }
+
+    for (TaskId dep : dependencies_) {
+      graph_.add_dependency(dep, task->id());
+    }
+
+    // Reset for next build
+    reset();
+
+    return task;
+  }
+
+private:
+  void reset() {
+    name_.clear();
+    device_ = DeviceType::Any;
+    priority_ = TaskPriority::Normal;
+    cpu_func_ = nullptr;
+    gpu_func_ = nullptr;
+    dependencies_.clear();
+  }
+
+  TaskGraph &graph_;
+  std::string name_;
+  DeviceType device_ = DeviceType::Any;
+  TaskPriority priority_ = TaskPriority::Normal;
+  Task::CpuFunction cpu_func_;
+  Task::GpuFunction gpu_func_;
+  std::vector<TaskId> dependencies_;
 };
 
 } // namespace hts
--- tests/test_task_builder.cpp
+++ tests/test_task_builder.cpp
@@ -1,105 +1,90 @@
-#include <gtest/gtest.h>
 #include "hts/task_builder.hpp"
+#include <gtest/gtest.h>
 
 using namespace hts;
 
 TEST(TaskBuilderTest, BasicBuild) {
-    TaskGraph graph;
-    TaskBuilder builder(graph);
-    
-    auto task = builder
-        .name("Test Task")
-        .device(DeviceType::CPU)
-        .cpu([](TaskContext& ctx) {})
-        .build();
-    
-    EXPECT_NE(task, nullptr);
-    EXPECT_EQ(task->name(), "Test Task");
-    EXPECT_EQ(task->preferred_device(), DeviceType::CPU);
-    EXPECT_TRUE(task->has_cpu_function());
+  TaskGraph graph;
+  TaskBuilder builder(graph);
+
+  auto task = builder.name("Test Task")
+                  .device(DeviceType::CPU)
+                  .cpu([](TaskContext &ctx) {})
+                  .build();
+
+  EXPECT_NE(task, nullptr);
+  EXPECT_EQ(task->name(), "Test Task");
+  EXPECT_EQ(task->preferred_device(), DeviceType::CPU);
+  EXPECT_TRUE(task->has_cpu_function());
 }
 
 TEST(TaskBuilderTest, WithPriority) {
-    TaskGraph graph;
-    TaskBuilder builder(graph);
-    
-    auto task = builder
-        .priority(TaskPriority::Critical)
-        .cpu([](TaskContext& ctx) {})
-        .build();
-    
-    EXPECT_EQ(task->priority(), TaskPriority::Critical);
+  TaskGraph graph;
+  TaskBuilder builder(graph);
+
+  auto task = builder.priority(TaskPriority::Critical)
+                  .cpu([](TaskContext &ctx) {})
+                  .build();
+
+  EXPECT_EQ(task->priority(), TaskPriority::Critical);
 }
 
 TEST(TaskBuilderTest, WithDependencies) {
-    TaskGraph graph;
-    TaskBuilder builder(graph);
-    
-    auto task1 = builder
-        .name("Task 1")
-        .cpu([](TaskContext& ctx) {})
-        .build();
-    
-    auto task2 = builder
-        .name("Task 2")
-        .after(task1)
-        .cpu([](TaskContext& ctx) {})
-        .build();
-    
-    auto predecessors = graph.get_predecessors(task2->id());
-    EXPECT_EQ(predecessors.size(), 1);
-    EXPECT_EQ(predecessors[0]->id(), task1->id());
+  TaskGraph graph;
+  TaskBuilder builder(graph);
+
+  auto task1 = builder.name("Task 1").cpu([](TaskContext &ctx) {}).build();
+
+  auto task2 =
+      builder.name("Task 2").after(task1).cpu([](TaskContext &ctx) {}).build();
+
+  auto predecessors = graph.get_predecessors(task2->id());
+  EXPECT_EQ(predecessors.size(), 1);
+  EXPECT_EQ(predecessors[0]->id(), task1->id());
 }
 
 TEST(TaskBuilderTest, MultipleDependencies) {
-    TaskGraph graph;
-    TaskBuilder builder(graph);
-    
-    auto task1 = builder.cpu([](TaskContext& ctx) {}).build();
-    auto task2 = builder.cpu([](TaskContext& ctx) {}).build();
-    
-    auto task3 = builder
-        .after(task1)
-        .after(task2)
-        .cpu([](TaskContext& ctx) {})
-        .build();
-    
-    auto predecessors = graph.get_predecessors(task3->id());
-    EXPECT_EQ(predecessors.size(), 2);
+  TaskGraph graph;
+  TaskBuilder builder(graph);
+
+  auto task1 = builder.cpu([](TaskContext &ctx) {}).build();
+  auto task2 = builder.cpu([](TaskContext &ctx) {}).build();
+
+  auto task3 =
+      builder.after(task1).after(task2).cpu([](TaskContext &ctx) {}).build();
+
+  auto predecessors = graph.get_predecessors(task3->id());
+  EXPECT_EQ(predecessors.size(), 2);
 }
 
 TEST(TaskBuilderTest, ResetAfterBuild) {
-    TaskGraph graph;
-    TaskBuilder builder(graph);
-    
-    auto task1 = builder
-        .name("Task 1")
-        .priority(TaskPriority::High)
-        .cpu([](TaskContext& ctx) {})
-        .build();
-    
-    // Second build should have default values
-    auto task2 = builder
-        .cpu([](TaskContext& ctx) {})
-        .build();
-    
-    EXPECT_EQ(task1->name(), "Task 1");
-    EXPECT_EQ(task1->priority(), TaskPriority::High);
-    
-    EXPECT_EQ(task2->name(), "");
-    EXPECT_EQ(task2->priority(), TaskPriority::Normal);
+  TaskGraph graph;
+  TaskBuilder builder(graph);
+
+  auto task1 = builder.name("Task 1")
+                   .priority(TaskPriority::High)
+                   .cpu([](TaskContext &ctx) {})
+                   .build();
+
+  // Second build should have default values
+  auto task2 = builder.cpu([](TaskContext &ctx) {}).build();
+
+  EXPECT_EQ(task1->name(), "Task 1");
+  EXPECT_EQ(task1->priority(), TaskPriority::High);
+
+  EXPECT_EQ(task2->name(), "");
+  EXPECT_EQ(task2->priority(), TaskPriority::Normal);
 }
 
 TEST(TaskBuilderTest, BothFunctions) {
-    TaskGraph graph;
-    TaskBuilder builder(graph);
-    
-    auto task = builder
-        .device(DeviceType::Any)
-        .cpu([](TaskContext& ctx) {})
-        .gpu([](TaskContext& ctx, cudaStream_t stream) {})
-        .build();
-    
-    EXPECT_TRUE(task->has_cpu_function());
-    EXPECT_TRUE(task->has_gpu_function());
+  TaskGraph graph;
+  TaskBuilder builder(graph);
+
+  auto task = builder.device(DeviceType::Any)
+                  .cpu([](TaskContext &ctx) {})
+                  .gpu([](TaskContext &ctx, cudaStream_t stream) {})
+                  .build();
+
+  EXPECT_TRUE(task->has_cpu_function());
+  EXPECT_TRUE(task->has_gpu_function());
 }
